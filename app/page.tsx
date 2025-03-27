5627e33676446205813394705a3058b77f59527c
viktor_strelianyi <chmpain@gmail.com>
2025-03-27 21:28:54 +0200

    - added shadcn - added prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import React from 'react';
 
 function PageHome() {
-	return (
-		<div>PageHome</div>
-	)
+  return (
+    <div className="flex flex-col gap-[20px] items-start justify-start h-screen p-[20px]">
+      <h1>PageHome</h1>
+      <Button
+        variant="default"
+        className="w-max"
+      >
+        Button
+      </Button>
+      <Input
+        placeholder="Search..."
+        className="w-max"
+      />
+    </div>
+  );
 }
 
-export default PageHome
+export default PageHome;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ['localhost:3000', '127.0.0.1:3000']
+  }
 };
 
 export default nextConfig;
